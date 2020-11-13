69177c9a6916d203b6c49f84200d3496420043df
Johan Gustafsson <johan@tinyrocket.se>
2020-11-13 15:46:14 +0100

    Simple request logging

---
+++ WebPrefer.SlackBot/Middleware/RequestResponseLoggingMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebPrefer.SlackBot.Middleware
+{
+    public class RequestResponseLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public RequestResponseLoggingMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
+        {
+            _next = next;
+            _logger = loggerFactory
+                      .CreateLogger<RequestResponseLoggingMiddleware>();
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            _logger.LogInformation($"{context.Request.Method} {context.Request.Path} {context.Request.QueryString}");
+            await _next(context);
+        }
+    }
+}
--- WebPrefer.SlackBot/Startup.cs
+++ WebPrefer.SlackBot/Startup.cs
@@ -16,6 +16,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using WebPrefer.SlackBot.Commands;
+using WebPrefer.SlackBot.Middleware;
 
 namespace WebPrefer.SlackBot
 {
@@ -41,12 +42,13 @@ namespace WebPrefer.SlackBot
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseRouting();
+            app.UseMiddleware<RequestResponseLoggingMiddleware>();
+
             app.UseSlackNet(c => c
                 .UseSigningSecret(Configuration["Slack:SigningSecret"])
                 .MapToPrefix("slack")
             );
-
-            app.UseRouting();
 
             var collection = new FontCollection();
             var fontFamily = collection.Install(env.GetFontPath("OpenSans-Bold.ttf"));
