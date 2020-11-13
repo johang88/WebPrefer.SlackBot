cd137b361641d5dabb628d8c120613a35582b735
Johan Gustafsson <johan@tinyrocket.se>
2020-11-13 14:33:12 +0100

    Configured azure logging

--- WebPrefer.SlackBot/Program.cs
+++ WebPrefer.SlackBot/Program.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.AzureAppServices;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +20,19 @@ namespace WebPrefer.SlackBot
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.AddAzureWebAppDiagnostics();
+                })
+                .ConfigureServices(serviceCollection =>
+                {
+                    serviceCollection.Configure<AzureFileLoggerOptions>(options =>
+                    {
+                        options.FileName = "azure-diagnostics-";
+                        options.FileSizeLimit = 50 * 1024;
+                        options.RetainedFileCountLimit = 5;
+                    });
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
